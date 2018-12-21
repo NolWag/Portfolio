7d35b7fdbaa6d1519355504021ed13bc32a84e10
NolWag <NolanDubyu@gmail.com>
2018-12-21 16:36:16 -0600

    styles

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,8 @@
 var port = document.getElementById('portfolio');
 var projects = document.getElementById('img');
 var next = document.getElementById('next');
+var hire = document.getElementById('hire');
+var info = document.getElementById('infoSection');
 
 port.addEventListener('click', function() {
     var classes = projects.firstElementChild.classList;
@@ -9,8 +11,11 @@ port.addEventListener('click', function() {
 });
 
 next.addEventListener('click', function() {
-  console.log('click')
   projects.appendChild(projects.firstElementChild);
   projects.lastElementChild.classList.add('hidden');
   projects.firstElementChild.classList.remove('hidden');
+});
+
+hire.addEventListener('click', function() {
+  info.classList.remove('hidden');
 });
